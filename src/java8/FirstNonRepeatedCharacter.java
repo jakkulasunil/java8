08cba020f5f778c645b5701f9759ec07b8433c12
suniljakkula <sunij8911@gmail.com>
2026-02-13 06:33:47 +0530

    non repeated character in a string

---
+++ src/java8/FirstNonRepeatedCharacter.java
@@ -0,0 +1,20 @@
+package java8;
+
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeatedCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String value = "Java articles are awesome";
+		Character result = value.chars().mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
+				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+				.entrySet().stream().filter(entry -> entry.getValue() == 1L).map(entry -> entry.getKey()).findFirst()
+				.get();
+		System.out.println("result : " + result);
+
+	}
+
+}
